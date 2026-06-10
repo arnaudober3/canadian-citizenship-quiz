49a4ce84690c8b5b63e797e7c04d4c90e1a30362
Arnaud Ober <arnaudober@Macbook-Air-de-Arnaud.local>
2026-06-10 19:32:45 +0200

    Revert "Merge branch 'main' into 'develop'"
    This reverts commit 9b8aeb75d5d468f546fd061d44df0cab9597b765, reversing
    changes made to ef2d915ae38076577fec27ef79578b81256b1408.

--- src/app/model.ts
+++
@@ -1,7 +0,0 @@
-export interface Question {
-    question: string;
-    options: [string, string, string, string]; // Four possible answers for each question
-    correctOptionIndex: number;
-}
-
-export const SET_SIZE = 20;
